7f8b5f194ee38a0c2327f6bcd05b32dd3267f006
JinhyeokLee <jinhyuklee@mymusictaste.com>
2023-06-12 16:00:23 +0900

    fix(prompt): Tweak prompt

--- src/lib/hooks/useChat.ts
+++ src/lib/hooks/useChat.ts
@@ -15,8 +15,9 @@ export type ChatMessage = {
 
 // todo: extract me
 const usePromptSchema = (model: string) => {
-  const initialText = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.`;
-  const promptHeader = "\nInstruction: ";
+  // const initialText = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.`;
+  const initialText = `Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.`;
+  const promptHeader = "\nInstruction:";
   const answerHeader = "\nResponse:";
   const promptFormat = `${promptHeader}{{prompt}}\n${answerHeader}`;
 
